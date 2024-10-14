f2283f31d456048e2914025ff0b0c8a8bd4955af
JD SOON <jdangelogsoon@gmail.com>
2024-10-15 02:08:05 +0800

    Established else statements

--- find_highest_number.py
+++ find_highest_number.py
@@ -10,3 +10,13 @@ def find_highest_number (variable1, variable2, variable3, variable4, variable5):
             if variable1>variable4:
                 if variable1>variable5:
                     print("Variable 1 is the Highest:", variable1)
+                else:
+                    print("Variable 5 is the Highest:", variable5)
+            else: 
+                print("Variable 4 is the Highest:", variable4)
+        else:
+            print("Variable 3 is the Highest:", variable3)
+    else: 
+        print("Variable 2 is the Highest: ", variable2)
+
+find_highest_number(variable1, variable2, variable3, variable4, variable5)
